485eee858d116628d5d0efe501bee8c7a0ce2dc2
csj <1660940271@qq.com>
2025-12-03 17:41:52 +0800

    新增工作台功能，包括今日数据查询、订单管理数据、菜品总览和套餐总览接口

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -83,4 +84,11 @@ public interface SetmealMapper {
      * @param ids
      */
     void deleteSetmealByIds(List<Long> ids);
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
